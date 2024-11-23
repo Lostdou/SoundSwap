65c5526ac91f6ac33b71fe883872163fb19de8c0
lostdou <facundobott@gmail.com>
2024-11-22 23:44:42 -0300

    new readme and i should read the docs

---
+++ README.md
@@ -0,0 +1,11 @@
+## SoundSwap
+
+Aplicación de escritorio para transferir playlists entre YT y Spotify (potencialmente otras plataformas tambien)
+
+## To Do
+
+- [x] Requestear las canciones desde spotify
+- [] Buscar esas canciones en YT
+- [] Crear una playlist en YT e insertar las canciones de spotify
+- [] El mismo proceso, pero al reves
+- [] Seria bueno una interfaz, no?
--- main.py
+++ main.py
@@ -19,16 +19,7 @@ sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
     scope='user-library-read'
 ))
 
-# Google credentials
-yt_credentials_path = 'path_to_your_google_credentials.json'
-scopes = ["https://www.googleapis.com/auth/youtube.force-ssl"]
-
-flow = InstalledAppFlow.from_client_secrets_file(yt_credentials_path, scopes)
-credentials = flow.run_console()
-
-youtube = build('youtube', 'v3', credentials=credentials)
-
-# Fetch liked songs from Spotify
+# Request de las canciones likeadas
 def fetch_spotify_liked_songs(limit):
     all_liked_songs = []
     offset = 0
@@ -40,63 +31,14 @@ def fetch_spotify_liked_songs(limit):
         offset += 50
     return all_liked_songs
 
-# Create YouTube playlist
-def create_youtube_playlist(youtube, title, description):
-    request = youtube.playlists().insert(
-        part="snippet,status",
-        body={
-            "snippet": {
-                "title": title,
-                "description": description
-            },
-            "status": {
-                "privacyStatus": "private"
-            }
-        }
-    )
-    response = request.execute()
-    return response["id"]
-
-# Search for song on YouTube
-def search_youtube_video(youtube, query):
-    request = youtube.search().list(
-        part="snippet",
-        q=query,
-        maxResults=1,
-        type="video"
-    )
-    response = request.execute()
-    if response['items']:
-        return response['items'][0]['id']['videoId']
-    return None
-
-# Add song to YouTube playlist
-def add_video_to_playlist(youtube, playlist_id, video_id):
-    youtube.playlistItems().insert(
-        part="snippet",
-        body={
-            "snippet": {
-                "playlistId": playlist_id,
-                "resourceId": {
-                    "kind": "youtube#video",
-                    "videoId": video_id
-                }
-            }
-        }
-    ).execute()
-
-# Transfer songs from Spotify to YouTube
-def transfer_songs_to_youtube(youtube, limit=50):
-    liked_songs = fetch_spotify_liked_songs(limit)
-    playlist_id = create_youtube_playlist(youtube, "My Spotify Liked Songs", "Songs I liked on Spotify transferred to YouTube")
-
-    for song in liked_songs:
-        track_name = song['track']['name']
-        artist_name = song['track']['artists'][0]['name']
-        search_query = f"{track_name} {artist_name}"
-        video_id = search_youtube_video(youtube, search_query)
-        if video_id:
-            add_video_to_playlist(youtube, playlist_id, video_id)
-
-# Transfer songs (specify the number of songs you want to transfer)
-transfer_songs_to_youtube(youtube, limit=50)
+# Request de las canciones de una playlist especifica
+def fetch_spotify_playlist_songs(playlist_id, limit):
+    all_playlist_songs = []
+    offset = 0
+    while len(all_playlist_songs) < limit:
+        playlist_songs = sp.playlist_tracks(playlist_id, limit=min(50, limit - len(all_playlist_songs)), offset=offset)
+        all_playlist_songs.extend(playlist_songs['items'])
+        if len(playlist_songs['items']) < 50:
+            break
+        offset += 50
+    return all_playlist_songs
